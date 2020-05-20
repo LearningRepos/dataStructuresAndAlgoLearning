b9193f21356147d655ba673614f0daf452d025cb
Mihir Achyuta <57376418+Mihir-Achyuta@users.noreply.github.com>
2020-05-19 21:26:18 -0700

    Recursion Section

---
+++ Recursion/RecursionIntro.java
@@ -0,0 +1,34 @@
+public class RecursionIntro{
+  public void CountDown(int number){
+    System.out.print(number + " ");
+    if(number>0){
+      number--;
+      CountDown(number);
+    }
+  }
+
+  public int SumRange(int number){
+    if(number == 0){
+      return 1;
+    }
+    return number + SumRange(number-1);
+  }
+
+  public int IterativeFactorial(int number){
+    int product = 1;
+    for(int i =1; i<=number; i++){
+      product*=i;
+    }
+    return product;
+  }
+
+  public int RecursiveFactorial(int number){
+    if(number == 0){
+      return 1;
+    }
+    else{
+      return number * RecursiveFactorial(number-1);
+    }
+  }
+
+}
