fc82e8cac93ecb13b162ed2023e4e89082769863
Mihir Achyuta <57376418+Mihir-Achyuta@users.noreply.github.com>
2020-06-29 14:26:52 -0700

    Binary Heaps And Priority Queues Section

--- Binary Heaps And Priority Queues/index.js
+++ Binary Heaps And Priority Queues/index.js
@@ -2,6 +2,10 @@
 //or
 //child node n and parent at index Math.floor((n-1)/2)
 
+//o(log n) insertions and deletions because of how the array is structured^
+//o(n) searching beacuse no particular strict order of elements just follow 1 rule : parent is always min/max of children
+//consider using a bianry search tree if you want to search in o(log n time) 
+
 class MaxBinaryHeap {
   constructor() {
     this.values = [];
@@ -34,9 +38,11 @@ class MaxBinaryHeap {
   }
 
   extractMax() {
+    //gets the last value in the top position
     var returnedMax = this.values.shift();
     this.values.unshift(this.values[this.values.length - 1]);
     this.values.pop();
+    //puts down the last value in the correct position
     this.bubbleDown();
     return returnedMax;
   }
@@ -158,7 +164,86 @@ class MinBinaryHeap {
   }
 }
 
+class Node{
+  constructor(value,priority){
+    this.value = value;
+    this.priority = priority;
+  }
+}
+
+class PriorityQueue{
+  constructor(){
+    this.values = [];
+  }
+
+  Enqueue(value, priority){
+    var newNode = new Node(value,priority);
+    this.values.push(newNode);
+    this.bubbleUp();
+  }
+
+  bubbleUp(){
+    var insertedIndex = this.values.length-1;
+    var insertedValue = this.values[insertedIndex].priority;
+    while(insertedIndex>0){
+      var parentIndex = Math.floor((insertedIndex-1)/2);
+      var parentValue = this.values[parentIndex].priority;
+      if(parentValue>insertedValue){
+        var temp = this.values[insertedIndex];
+        var temp2 = this.values[parentIndex];
+        this.values[insertedIndex] = temp2;
+        this.values[parentIndex] = temp;
+      }
+      insertedIndex = parentIndex;
+    }
+  }
+
+  Dequeue(){
+    var removedNode = this.values.shift();
+    this.values.unshift(this.values[this.values.length - 1]);
+    this.values.pop();
+    this.bubbleDown();
+    return removedNode;
+  }
+
+  bubbleDown(){
+    //gets the accidental index and value of it along with array length
+    var accidentalIndex = 0;
+    var accidentalValue = this.values[accidentalIndex].priority;
+    var length = this.values.length;
+    while(true){
+      //defines children indices but not values
+      var leftChildIndex = accidentalIndex*2+1;
+      var rightChildIndex = accidentalIndex*2+2;
+      var swap = null;
+      //checks to see if indices valid
+      var leftChildValue,rightChildValue; 
+      if(leftChildIndex < length){
+        leftChildValue = this.values[leftChildIndex].priority;
+        if(leftChildValue<accidentalValue){
+          swap = leftChildIndex;
+        }
+      }
+      if(rightChildIndex < length){
+        rightChildValue = this.values[rightChildIndex].priority;
+        if(swap === null && rightChildValue<accidentalValue || swap !== null && rightChildValue < leftChildValue){
+          swap = rightChildIndex;
+        }
+      }
+      if(swap === null){
+        break;
+      }
+      var temp1 = this.values[accidentalIndex];
+      var temp2 = this.values[swap];
+      this.values[accidentalIndex] = temp2;
+      this.values[swap] = temp1;
+      accidentalIndex = swap;
+    }
+  }
+}
+
 var maxHeap = new MaxBinaryHeap();
+console.log("Inserting Values in MaxHeap");
 maxHeap.Insert(41);
 maxHeap.Insert(39);
 maxHeap.Insert(33);
@@ -169,11 +254,13 @@ maxHeap.Insert(55);
 console.log(maxHeap);
 console.log();
 
+console.log("Deletes max in MaxHeap");
 console.log(maxHeap.extractMax());
 console.log(maxHeap);
 console.log();
 
 var minHeap = new MinBinaryHeap();
+console.log("Inserting Values in MinHeap");
 minHeap.Insert(41);
 minHeap.Insert(39);
 minHeap.Insert(33);
@@ -184,8 +271,22 @@ minHeap.Insert(55);
 console.log(minHeap);
 console.log();
 
+console.log("Deletes min in MinHeap");
 minHeap.extractMin();
 console.log(minHeap);
+console.log();
 
+var priority = new PriorityQueue();
+console.log("Adds nodes to priority queue with different priorities");
+priority.Enqueue("Gunshot Wound",1);
+priority.Enqueue("Fever",5);
+priority.Enqueue("Exploded Head",0);
+priority.Enqueue("Concussion",2);
+priority.Enqueue("Drunk",3);
+priority.Enqueue("Dead Tissue",4);
+console.log(priority);
+console.log();
 
-
+console.log("Removes the most prioritized node from the priority queue");
+priority.Dequeue();
+console.log(priority);
