a3917972f2dafe948c9ac07722ef0023385b37a4
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-02-26 01:43:59 -0800

    Added merge sort

--- JavaDSA/Sorts/Merge Sort/Merge.java
+++ JavaDSA/Sorts/Merge Sort/Merge.java
@@ -1,55 +1,52 @@
-import java.util.Arrays; 
+import java.util.Arrays;
+
 public class Merge {
-
-  public static int[] Merge(int[] array1, int[] array2){
-    int[] mergedArray = new int[array1.length + array2.length]; //array length of 7
-    int i = 0;//index of array1
-    int j = 0;//index of array2
-    int k = 0;//separate index for merged array adding
-
-    while(i<array1.length && j< array2.length){ //fills up at least 1 subarray
-      if(array1[i] < array2[j]){
-        mergedArray[k] = array1[i];
-        k++;
-        i++;
-      }
-      else if(array1[i] > array2[j]){
-        mergedArray[k] = array2[j];
-        k++;
-        j++;
-      }
-    }
-    //fills the other array up now
-    while(i<array1.length){
-      mergedArray[k] = array1[i];
-      k++;
-      i++;
-    }
-    while(j<array2.length){
-      mergedArray[k] = array2[j];
-      k++;
-      j++;
-    }
-    return mergedArray;
+  public static void main(String args[]) {
+    int[] array = { 1, 3, 9, 7, 4, 8 };
+    int[] newArray = mergesort(array);
+    System.out.println(Arrays.toString(newArray));
   }
 
-  public static int[] MergeSort(int[] array){
-    if(array.length <= 1){
-      return array;
+  public static int[] mergesort(int[] arr) {
+    if (arr.length <= 1) {
+      return arr;
     }
-    int midpoint = array.length/2;
-    //breaks array apart and returns each individual piece of array then merges it at the end
-    int[] LeftArray = MergeSort(Arrays.copyOfRange(array, 0, midpoint));
-    int[] RightArray = MergeSort(Arrays.copyOfRange(array, midpoint, array.length));
-    return Merge(LeftArray,RightArray);
+    int[] leftSplit = mergesort(Arrays.copyOfRange(arr, 0, arr.length / 2));
+    int[] rightSplit = mergesort(Arrays.copyOfRange(arr, arr.length / 2, arr.length));
+    return merge(leftSplit, rightSplit);
   }
 
-  public static void main(String[] args) {
-    int[] array = {10,24,76,73,72,59};
-    int[] MergedArray = MergeSort(array);
-    //implementation of Merge Sort
-    for(int i =0; i<MergedArray.length; i++){
-      System.out.println(MergedArray[i]);
+  public static int[] merge(int[] leftArr, int[] rightArr) {
+    int[] newArray = new int[leftArr.length + rightArr.length];
+    int newArrayIndex = 0;
+    int leftArrayIndex = 0;
+    int rightArrayIndex = 0;
+
+    while (leftArrayIndex < leftArr.length && rightArrayIndex < rightArr.length) {
+      if (leftArr[leftArrayIndex] < rightArr[rightArrayIndex]) {
+        newArray[newArrayIndex] = leftArr[leftArrayIndex];
+        newArrayIndex++;
+        leftArrayIndex++;
+      } else {
+        newArray[newArrayIndex] = rightArr[rightArrayIndex];
+        newArrayIndex++;
+        rightArrayIndex++;
+      }
     }
+
+    if (leftArrayIndex < leftArr.length) {
+      for (int i = leftArrayIndex; i < leftArr.length; i++) {
+        newArray[newArrayIndex] = leftArr[leftArrayIndex];
+        newArrayIndex++;
+        leftArrayIndex++;
+      }
+    } else {
+      for (int i = rightArrayIndex; i < rightArr.length; i++) {
+        newArray[newArrayIndex] = rightArr[rightArrayIndex];
+        newArrayIndex++;
+        rightArrayIndex++;
+      }
+    }
+    return newArray;
   }
 }
