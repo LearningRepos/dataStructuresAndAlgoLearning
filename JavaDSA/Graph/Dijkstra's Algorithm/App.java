2b1ab8387dd364a461d2cf454f9a9e1c64f60f35
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-07-11 23:34:53 -0700

    Added djikstra's algorithm to the weighted graph in Java

--- JavaDSA/Graph/Dijkstra's Algorithm/App.java
+++ JavaDSA/Graph/Dijkstra's Algorithm/App.java
@@ -4,18 +4,67 @@ import java.util.stream.Collectors;
 public class App {
     public static void main(String[] args) {
         WeightedGraph g = new WeightedGraph();
-        g.addVertex("USA");
-        g.addVertex("Mexico");
-        g.addVertex("Canada");
-        g.addEdge("USA", "Mexico", 10);
-        g.addEdge("USA", "Canada", 20);
-//        g.deleteEdge("USA", "Canada");
-//        g.deleteVertex("USA");
-        System.out.println(g.graph);
+        g.addVertex("A");
+        g.addVertex("B");
+        g.addVertex("C");
+        g.addVertex("D");
+        g.addVertex("E");
+        g.addVertex("F");
+        g.addEdge("A", "B", 4);
+        g.addEdge("A", "C", 2);
+        g.addEdge("B", "E", 3);
+        g.addEdge("C", "D", 2);
+        g.addEdge("D", "E", 3);
+        g.addEdge("C", "F", 4);
+        g.addEdge("D", "F", 1);
+        g.addEdge("F", "E", 1);
+
+        g.dijkstraAlgo("A", "E");
+    }
+
+    public static class Tuple {
+        String Node;
+        int distance;
+
+        Tuple(String Node, int distance) {
+            this.Node = Node;
+            this.distance = distance;
+        }
     }
 
     public static class WeightedGraph {
         HashMap<String, ArrayList<HashMap>> graph = new HashMap<>();
+
+        public void dijkstraAlgo(String startVertex, String endVertex) {
+            HashMap<String, Integer> distances = new HashMap<>();
+            HashMap<String, String> previousNodes = new HashMap<>();
+            PriorityQueue<Tuple> q = new PriorityQueue<>((Tuple t1, Tuple t2) -> t1.distance - t2.distance);
+
+            distances.put(startVertex, 0);
+            previousNodes.put(startVertex, null);
+            q.offer(new Tuple(startVertex, 0));
+
+            while (!q.isEmpty()) {
+                Tuple currTuple = q.poll();
+                if (currTuple.Node.equals(endVertex)) {
+                    break;
+                } else {
+                    List<HashMap> vertexList = graph.get(currTuple.Node);
+
+                    for (HashMap neighbor : vertexList) {
+                        int distance = (int) neighbor.get("weight") + distances.get(currTuple.Node);
+
+                        if (!distances.containsKey(neighbor.get("node")) || distance < distances.get(neighbor.get("node"))) {
+                            distances.put((String) neighbor.get("node"), distance);
+                            previousNodes.put((String) neighbor.get("node"), currTuple.Node);
+                            q.add(new Tuple((String) neighbor.get("node"), distance));
+                        }
+                    }
+                }
+            }
+            System.out.println(distances);
+            System.out.println(previousNodes);
+        }
 
         public void addVertex(String vertex) {
             if (!graph.containsKey(vertex)) graph.put(vertex, new ArrayList<>());
